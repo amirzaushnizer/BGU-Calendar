d88f1e91ea93fce66b6b7974b73b3029d070c8cd
Amir Zaushnizer <78536626+amirzaushnizer@users.noreply.github.com>
2022-03-13 23:04:59 +0200

    Introduce Webpack modules (#3)
    * try add modules - doesn't work yet
    
    * add ignore file
    
    * introduce modules

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import { createCalendarFile } from "./helpers/ics-helper";
+import { download } from "./helpers/download-helper";
+import { parseCalendarHTML } from "./helpers/calendar-html-parser";
+
+const getCalendarFileStr = (table) => {
+  const classEvents = parseCalendarHTML(table);
+  return createCalendarFile(classEvents);
+};
+
+const clickOnDownloadHandler = (e) => {
+  e.preventDefault();
+
+  const table = getCalendarTable();
+  const calendarFileStr = getCalendarFileStr(table);
+  download("bgu_calendar.ics", calendarFileStr);
+};
+
+const getCalendarTable = () => {
+  return [...document.querySelector(".standardLook").children[0].children];
+};
+
+/*
+  main script
+ */
+const title = document.querySelector(".uRegionHeading");
+const isHourTablePage = title && title.textContent.includes("מערכת שעות");
+
+const createDownloadButton = () => {
+  const btn = document.createElement("button");
+  btn.addEventListener("click", clickOnDownloadHandler);
+  btn.innerHTML = "הורד מערכת שעות";
+  return btn;
+};
+
+if (isHourTablePage) {
+  const actionBar = document.getElementsByClassName(
+    "uRegionContent clearfix"
+  )[0];
+  if (actionBar) {
+    const btn = createDownloadButton();
+    actionBar.appendChild(btn);
+  }
+}
